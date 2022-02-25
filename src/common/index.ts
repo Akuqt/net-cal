9054a7b4a40c57737adc424ff47ae7137df68328
Italo Alfaro <bestor911@gmail.com>
2022-02-25 13:22:53 -0500

    feat: ipv4 implementation

---
+++ src/common/index.ts
@@ -0,0 +1,4 @@
+export * from "./binary";
+export * from "./ipv4";
+export * from "./parser";
+export * from "./types";
---
+++ src/common/ipv4.ts
@@ -0,0 +1,59 @@
+import { AddressFormat, IPv4Base } from "./types";
+import { int2bin } from "./binary";
+import { parser } from "./parser";
+import { FormatResult } from ".";
+
+export class IPv4 {
+  private mask_: number[];
+  private prefix_: number;
+  private address_: number[];
+  private wildcard_: number[];
+  constructor(ipv4: IPv4Base) {
+    const { address, mask, prefix, wildcard } = parser(ipv4);
+    this.mask_ = mask;
+    this.prefix_ = prefix;
+    this.address_ = address;
+    this.wildcard_ = wildcard;
+  }
+
+  private formater(
+    format: AddressFormat = "plainDecimals",
+    address: number[]
+  ): FormatResult {
+    switch (format) {
+      case "plainDecimals":
+        return address.join(".");
+      case "plainBits":
+        return address.map((el) => int2bin(el)).join(".");
+      case "decimalOctets":
+        return address;
+      case "bitOctets":
+        return address.map((el) => int2bin(el));
+      default:
+        return address.join(".");
+    }
+  }
+
+  public toString(full?: boolean): string {
+    if (full) {
+      return this.address() + " " + this.mask();
+    }
+    return this.address() + "/" + this.prefix_;
+  }
+
+  public address(format: AddressFormat = "plainDecimals") {
+    return this.formater(format, this.address_);
+  }
+
+  public mask(format: AddressFormat = "plainDecimals") {
+    return this.formater(format, this.mask_);
+  }
+
+  public wildcard(format: AddressFormat = "plainDecimals") {
+    return this.formater(format, this.wildcard_);
+  }
+
+  public prefix() {
+    return this.prefix_;
+  }
+}
