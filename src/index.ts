b2dde49cc2a36cd23234b5257b57b5c2295117b0
Italo Alfaro <bestor911@gmail.com>
2022-02-26 19:21:34 -0500

    chore: add modules to export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-import { IPv4 } from "./common";
-
-const ip = "192.0.0.1/17";
-const ipv4 = new IPv4(ip);
-
-console.log(ipv4.toString());
-console.log(ipv4.toString(true));
-console.log(ipv4.address("plainBits"));
-console.log(ipv4.address("bitOctets"));
-console.log(ipv4.address("plainDecimals"));
-console.log(ipv4.address("decimalOctets"));
+export * from "./calc";
+export * from "./common";
