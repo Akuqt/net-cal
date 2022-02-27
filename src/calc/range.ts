1a169b1831d19e81221dd3c2b046a1ddcad3bcb6
Italo Alfaro <bestor911@gmail.com>
2022-02-26 21:01:01 -0500

    fix: invalid first ip in range

--- src/calc/range.ts
+++ src/calc/range.ts
@@ -1,6 +1,9 @@
 import {
   IPv4,
   IPv4Base,
+  getMagic,
+  getOctets,
+  increment,
   RangeResult,
   IPv4Address,
   fromPlainBits,
@@ -27,11 +30,6 @@ const range_ = (ip: IPv4): RangeResult => {
   const addressBits = ip.address("plainBits") as string;
   const wildcard = ip.wildcard("plainBits") as string;
   const mask = ip.mask("plainBits") as string;
-  let first = "";
-  for (let i = 0; i < addressBits.length; i++) {
-    if (i < addressBits.length - 1) first += addressBits[i];
-    else first += parseInt(addressBits[i]) | 1;
-  }
   const last = base_(
     addressBits,
     wildcard.slice(0, wildcard.length - 1) + "0",
@@ -47,7 +45,10 @@ const range_ = (ip: IPv4): RangeResult => {
     network,
     prefix: ip.prefix(),
     mask: ip.mask("plainDecimals") as IPv4Address,
-    first: fromPlainBits(first),
+    first: increment(getOctets(network), {
+      value: 1,
+      octet: 3,
+    }).join(".") as IPv4Address,
     last,
     broadcast,
     wildcard: ip.wildcard("plainDecimals") as IPv4Address,
