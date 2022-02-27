7a46e59bb6ebf6c6691d5c77e976ff64789f1d6c
Italo Alfaro <bestor911@gmail.com>
2022-02-26 19:16:29 -0500

    refactor: move some functions to `util`

--- src/common/parser.ts
+++ src/common/parser.ts
@@ -1,42 +1,7 @@
-import { IPv4Address, IPv4Base, ValidationResult } from "./types";
-import { bin2int, int2bin } from "./binary";
-import { ParserResult } from ".";
+import { IPv4Address, IPv4Base, ValidationResult, ParserResult } from "./types";
+import { createMask, getOctets, getPrefix } from "./util";
 
-const getOctets = (base: IPv4Address) => {
-  return base.split(".").map((oc) => parseInt(oc));
-};
-
-const getPrefix = (mask: IPv4Address) => {
-  const txt = mask
-    .split(".")
-    .map((int) => int2bin(int))
-    .join("");
-  return (txt.match(/1/g) || []).length;
-};
-
-const createMask = (prefix: number | string) => {
-  if (typeof prefix === "string") {
-    prefix = parseInt(prefix);
-  }
-  if (prefix > 32 || prefix < 0) {
-    throw new Error("Invalid mask prefix.");
-  }
-  const rawBits = new Array<number>(32).fill(1, 0, prefix).fill(0, prefix, 32);
-  return rawBits
-    .reduce((acc, curr, i) => {
-      if (i % 8 === 0 && i > 0) {
-        acc += `.${curr}`;
-      } else {
-        acc += curr;
-      }
-      return acc;
-    }, "")
-    .split(".")
-    .map((oc) => bin2int(oc))
-    .join(".");
-};
-
-const validate = (ipv4: IPv4Address) => {
+const validate = (ipv4: IPv4Address): ValidationResult => {
   const octets = getOctets(ipv4);
   let valid = true;
   if (octets.length > 4) {
@@ -86,7 +51,7 @@ export const parser = (ipv4: IPv4Base) => {
   if (typeof ipv4 === "string") {
     const [address_, prefix] = ipv4.split("/");
     address = validate(address_ as IPv4Address);
-    mask = validate(createMask(prefix) as IPv4Address);
+    mask = validate(createMask(prefix).plain as IPv4Address);
   } else if (typeof ipv4 === "object") {
     address = validate(ipv4.address);
     mask = validate(ipv4.mask);
