7cc37d6e73518c98061a56eefa82e448338a695b
Italo Alfaro <bestor911@gmail.com>
2022-02-26 19:13:54 -0500

    feat: new types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,3 +1,11 @@
+export interface IPv4T {
+  toString: (full?: boolean) => string;
+  address: (format: AddressFormat) => FormatResult;
+  mask: (format: AddressFormat) => FormatResult;
+  wildcard: (format: AddressFormat) => FormatResult;
+  prefix: () => number;
+}
+
 export type IPv4Address = `${number}.${number}.${number}.${number}`;
 
 export interface ExplicitIPv4Address {
@@ -28,3 +36,32 @@ export type AddressFormat =
   | "plainBits"
   | "decimalOctets"
   | "bitOctets";
+
+export interface Magic {
+  value: number;
+  octet: number;
+}
+
+export interface RangeResult {
+  network: IPv4Address;
+  prefix: number;
+  mask: IPv4Address;
+  first: IPv4Address;
+  last: IPv4Address;
+  broadcast: IPv4Address;
+  wildcard: IPv4Address;
+}
+
+export interface VLSMResult {
+  result: {
+    fullNetwork: IPv4T;
+    range: RangeResult;
+  }[];
+  required: number;
+  available: number;
+}
+
+export interface SubnetResult {
+  fullNetwork: IPv4T;
+  range: RangeResult;
+}
